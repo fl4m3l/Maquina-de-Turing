8e8bf5d97c1d28d32aeac394f12667a083d59d1a
Daniel Oliveira <fl4m3l@users.noreply.github.com>
2016-11-06 00:51:06 -0300

    Update TesteMesmaQuantidadeDeAeBeC.java

--- test/testes/TesteMesmaQuantidadeDeAeBeC.java
+++ test/testes/TesteMesmaQuantidadeDeAeBeC.java
@@ -13,7 +13,7 @@ public class TesteMesmaQuantidadeDeAeBeC {
     public void criarMT() {
         String estados = "q0, q1, q2, q3, q4, q5";
         String simbolosDoAlfabeto = "a, b, c";
-        String simbolosDaFita = "a, b, x, y, z, $";
+        String simbolosDaFita = "a, b, c, x, y, z, $";
         String estadoInicial = "q0";
         String funcaoDeTransicao = "q0-a/q1-x-D, q1-a/q1-a-D, q1-b/q2-y-D, q2-b/q2-b-D, q2-c/q3-z-E, "
                 + "q3-b/q3-b-E, q3-y/q3-y-E, q3-a/q3-a-E, q3-x/q0-x-D, q1-y/q1-y-D, q2-z/q2-z-D, q3-z/q3-z-E, "
